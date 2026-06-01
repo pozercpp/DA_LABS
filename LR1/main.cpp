8f7001f489fa22ea0b7592a03c0ea0393e222713
pozercpp <n.a.chigrin@yandex.ru>
2026-06-02 02:05:38 +0300

    жизнь хуйня

--- LR1/main.cpp
+++ LR1/main.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
+#include<iostream>
+#include<limits>
 
-template <class T>
-class MyVector
-{
+template<typename T>
+class DynamicArray {
 private:
-    size_t sz;
-    size_t capacity;
-    T* data = nullptr;
-
+    T* data_ptr = nullptr;
+    size_t sz = 0;
+    size_t cap = 0;
 public:
     class iterator {
     public:
@@ -72,304 +71,262 @@ public:
     };
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-    
-    ~MyVector() noexcept {
-        delete[] data;
-    	data = nullptr;
-    	sz = 0;
-    	capacity = 0;
+    DynamicArray() {}
+    DynamicArray(size_t sz) {
+        resize(sz);
     }
-    
-    void pop()
-    {
-        if (sz > 0) sz--;
-    }
-    
-    void clear() {
-        sz = 0;
-    }
-
-    void push_back(const T& n)
-    {
-        if (sz >= capacity) {
-            reallocate(capacity ? capacity * 2 : 1);
+    DynamicArray(size_t sz, T default_val) {
+        resize(sz);
+        for (size_t i = 0; i < sz; ++i) {
+            data_ptr[i] = default_val;
         }
-        data[sz] = n;
-        sz++;
     }
-    
-    void push_back(T&& n)
-    {
-        if (sz >= capacity) {
-            reallocate(capacity ? capacity * 2 : 1);
-        }
-        data[sz] = std::move(n);
-        sz++;
-    }
-    
-    MyVector() 
-        : sz(0), capacity(0), data(nullptr){};
-    
-    MyVector(const size_t& n) 
-        : sz(n), capacity(n), data(new T[n]) {}
-
-    MyVector(const size_t& n, const T& elem) 
-        : sz(n), capacity(n), data(new T[n]) {
-            for (size_t i = 0; i < n; ++i) {
-                data[i] = elem;
+    DynamicArray(const std::initializer_list<T>& params) {
+        if (params.size() > 0) {
+            resize(params.size());
+            size_t i = 0;
+            for (auto elem : params) {
+                data_ptr[i] = std::move(elem);
+                ++i;
             }
         }
-    
-    MyVector(const std::initializer_list<T>& t)
-        : sz(0), capacity(t.size()), data(new T[t.size()])
-    {
-        for (const T& i : t) {
-            push_back(i);
-        }
     }
-    
-    MyVector(const MyVector<T>& other)
-        : sz(other.sz), capacity(other.capacity), data(new T[other.capacity])
-    {
-        std::copy(other.data, other.data + other.sz, data);
-    }
-    
-    MyVector(MyVector<T>&& other) noexcept
-        : sz(other.sz), capacity(other.capacity), data(other.data)
-    {
-        other.data = nullptr;
-        other.sz = 0;
-        other.capacity = 0;
-    }
-    
-    void erase(size_t ind) {
-        for (size_t i = ind; i < sz - 1; ++i) {
-            data[i] = data[i + 1];
-        }
-        sz--;
-    }
-    
-    void reallocate(size_t newsize) {
-        if (newsize == 0) newsize = 1;
-        T* newdata = new T[newsize];
-        if (data) {
+    DynamicArray(const DynamicArray& other) {
+        if (this != &other) {
+            resize(other.sz);
             for (size_t i = 0; i < sz; ++i) {
-                newdata[i] = std::move(data[i]);
+                data_ptr[i] = other.data_ptr[i];
             }
-            delete[] data;
         }
-        data = std::move(newdata);
-        capacity = newsize;
     }
-    
-    void resize(size_t newsize) {
-        if (newsize > sz) {
-            reallocate(newsize);
+    DynamicArray(DynamicArray&& other) noexcept {
+        if (this != &other) {
+            data_ptr = other.data_ptr;
+            sz = other.sz;
+            cap = other.cap;
+            other.data_ptr = nullptr;
+            other.sz = 0;
+            other.cap = 0;
         }
-        sz = newsize;
     }
-    
-    T& operator[](size_t ind) {
-        return data[ind];
+    ~DynamicArray() noexcept {
+        sz = cap = 0;
+        delete[] data_ptr;
+        data_ptr = nullptr;
     }
-    
-    const T& operator[](size_t ind) const {
-        return data[ind];
+    T& operator[](size_t idx) {
+        return data_ptr[idx];
     }
-    
-    const size_t size() const {
+    const T operator[](size_t idx) const {
+        return data_ptr[idx];
+    }
+    void reserve(size_t new_cap) {
+    if (new_cap <= cap) return;
+        auto new_mem = new T[new_cap];
+        for (size_t i = 0; i < sz; ++i) {
+            new_mem[i] = std::move(data_ptr[i]);
+        }
+        delete[] data_ptr;
+        data_ptr = new_mem;
+        cap = new_cap;
+    }
+    void resize(size_t new_sz) {
+        if (new_sz < sz) {
+            for (size_t i = new_sz; i < sz; ++i) data_ptr[i].~T();
+        }
+        if (new_sz > cap) {
+            reserve(std::max(new_sz, cap * 2));
+        }
+        if (new_sz > sz) {
+            for (size_t i = sz; i < new_sz; ++i) {
+                new (&data_ptr[i]) T();
+            }
+        }
+        sz = new_sz;
+    }
+    DynamicArray& operator=(const DynamicArray& other) {
+        if (this != &other) {
+            delete[] data_ptr;
+            data_ptr = new T[other.cap];
+            sz = other.sz;
+            cap = other.cap;
+            for (size_t i = 0; i < sz; ++i) {
+                data_ptr[i] = other.data_ptr[i];
+            }
+        }
+        return *this;
+    }
+    DynamicArray& operator=(DynamicArray&& other) noexcept {
+        if (this != &other) {
+            data_ptr = other.data_ptr;
+            sz = other.sz;
+            cap = other.cap;
+            other.data_ptr = nullptr;
+            other.sz = 0;
+            other.cap = 0;
+        }
+
+        return *this;
+    }
+    size_t size() const {
         return sz;
     }
-    
-    MyVector<T>& operator=(const MyVector<T>& other) {
-        if (this == &other) {
-            return *this;
-        }
-        delete[] data;
-    
-        capacity = other.capacity;
-        sz = other.sz;
-        data = new T[capacity];
-    
-        std::copy(other.data, other.data + other.sz, data);
-    
-        return *this;
+    template<typename U>
+    void push_back(U&& val) {
+        resize(sz + 1);
+        data_ptr[sz - 1] = std::forward<U>(val);
     }
-
-    MyVector<T>& operator=(MyVector<T>&& other) {
-        if (this != &other) {
-            delete[] data;
-            data = other.data;
-            sz = other.sz;
-            capacity = other.capacity;
-            other.data = nullptr;
-            other.sz = 0;
-            other.capacity = 0;
-        }
-        return *this;
+    T pop_back() {
+        --sz;
+        return data_ptr[sz];
     }
+    template<typename U>
+    void insert(size_t pos, U&& val) {
+        resize(sz + 1);
+        for (size_t i = sz - 1; i > pos; --i) {
+            data_ptr[i] = std::move(data_ptr[i - 1]);
+        }
+        data_ptr[pos] = std::forward<U>(val);
+    }
+    void remove(size_t pos) {
+        for (size_t i = pos; i < sz - 1; ++i) {
+            data_ptr[i] = std::move(data_ptr[i + 1]);;
+        }
+        resize(sz - 1);
+    }
+    void clear() noexcept {
+        for (size_t i = 0; i < sz; ++i) {
+            data_ptr[i] = T{};
+        }
 
-    iterator begin() noexcept { return iterator(data); }
-    iterator end() noexcept { return iterator(data + sz); }
-    const_iterator begin() const noexcept { return const_iterator(data); }
-    const_iterator end() const noexcept { return const_iterator(data + sz); }
-    const_iterator cbegin() const noexcept { return const_iterator(data); }
-    const_iterator cend() const noexcept { return const_iterator(data + sz); }
+        sz = 0;
+    }
+    iterator begin() noexcept { return iterator(data_ptr); }
+    iterator end() noexcept { return iterator(data_ptr + sz); }
+    const_iterator begin() const noexcept { return const_iterator(data_ptr); }
+    const_iterator end() const noexcept { return const_iterator(data_ptr + sz); }
+    const_iterator cbegin() const noexcept { return const_iterator(data_ptr); }
+    const_iterator cend() const noexcept { return const_iterator(data_ptr + sz); }
     reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
     reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
     const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
     const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
     const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(end()); }
     const_reverse_iterator crend() const noexcept { return const_reverse_iterator(begin()); }
-    
+    iterator upper_boud(iterator start, iterator end, const T& val) const noexcept {
+        auto count = std::distance(start, end);
+        while (count > 0) {
+            auto step = count / 2;
+            auto it = start + step;
+            if (!(val < *it)) {
+                start = ++it;
+                count -= step + 1;
+            } else {
+                count = step;
+            }
+        }
+        return start;
+    }
+    template<typename U>
+    void add_sorted(U&& val) {
+        auto pos = upper_boud(begin(), end(), val) - begin();
+        insert(pos, std::forward<U>(val));
+    }
 };
 
-using String = MyVector<char>;
-
-struct node {
-    String key;
-    String val;
-    node() = default;
-    node(const node& other) : key(other.key), val(other.val) {}
-    node(node&& other) : key(std::move(other.key)), val(std::move(other.val)) {}
-    ~node() noexcept = default;
-    node& operator =(const node& other) {
+struct elem {
+    size_t key;
+    DynamicArray<char> val;
+    ~elem() noexcept = default;
+    bool operator<(const elem& other) const {
+        return key < other.key;
+    }
+    bool operator>(const elem& other) const {
+        return key > other.key;
+    }
+    bool operator<=(const elem& other) const {
+        return key <= other.key;
+    }
+    bool operator>=(const elem& other) const {
+        return key >= other.key;
+    }
+    bool operator==(const elem& other) const {
+        return key == other.key;
+    }
+    elem() : key(0), val() {}
+    elem(size_t k, DynamicArray<char> v) : key(k), val(std::move(v)) {}
+    elem(const elem& other) : key(other.key), val(other.val) {}
+    elem(elem&& other) : key(other.key), val(std::move(other.val)) {}
+    elem operator =(const elem& other) {
         key = other.key;
-        val = other.val; 
+        val = other.val;
         return *this;
     }
-    node& operator =(node&& other) {
-        key = std::move(other.key);
-        val = std::move(other.val); 
+    elem operator =(elem&& other) { 
+        key = other.key;
+        val = std::move(other.val);
         return *this;
     }
-};
-
-struct date {
-    size_t day, month, year;
-};
-
-std::size_t find(const String& line, char c, std::size_t start = 0) {
-    for (std::size_t i = start; i < line.size(); ++i) {
-        if (line[i] == c) {
-            return i;
-        }
-    }
-    return line.size();
-}
-
-String substr(const String& line, std::size_t start, std::size_t end) {
-    String result;
-    for (std::size_t i = start; i < end && i < line.size(); ++i) {
-        result.push_back(line[i]);
-    }
-    return result;
-}
-
-int stoi(const String& line) {
-    int result = 0;
-    for (std::size_t i = 0; i < line.size(); ++i) {
-        result = result * 10 + (line[i] - '0');
-    }
-    return result;
-}
-
-date date_to_numb(const String& s) {
-    size_t day = 0, month = 0, year = 0;
-    date d;
-    int part = 0;
-    for (size_t i = 0; i < s.size(); ++i) {
-        if (s[i] == '.') {
-            part++;
-            continue;
-        }
-        if (part == 0)
-            d.day = day * 10 + (s[i] - '0');
-        else if (part == 1)
-            d.month = month * 10 + (s[i] - '0');
-        else
-            d.year = year * 10 + (s[i] - '0');
-    }
-    return d;
-}
-
-enum dates {
-    day = 0,
-    month = 1,
-    year = 2
 };
 
 template<typename iterator>
-size_t get_key(iterator it, dates fl) {
-    if (fl == day) {
-        return it->day;
-    }
-    if (fl == month) {
-        return it->month;
-    }
-    return it->year;
+size_t get_key(iterator it) {
+    return it->key;
 }
 
 template<typename iterator>
-void count_sort(iterator start, iterator end, dates fl) {
-    if (start == end) return;
-    DynamicArray<size_t> cnt(65536);
+void bucket_sort(iterator start, iterator end) {
+	if (start == end) {
+    	return;
+    }
+    auto min = get_key(start);
+    auto max = get_key(start);
+    size_t dist = static_cast<size_t>(std::distance(start, end));
     for (auto it = start; it != end; ++it) {
-        ++cnt[get_key(it, fl)];
+        if (min > get_key(it)) {
+            min = get_key(it);
+        }
+        if (max < get_key(it)) {
+            max = get_key(it);
+        }
     }
-    for (size_t i = 0; i + 1 < cnt.size(); ++i) {
-        cnt[i + 1] += cnt[i];
+    size_t range = max - min;
+    auto get_ind = [&](size_t key) -> size_t {
+        double d = static_cast<double>(key - min) / range;
+        return static_cast<size_t>(static_cast<double>(dist - 1) * d);
+    };
+    DynamicArray<DynamicArray<elem>> buckets(dist);
+    for (auto it = start; it != end; ++it) {
+        auto& vec = buckets[get_ind(get_key(it))];
+        vec.add_sorted(std::move(*it));
     }
-    DynamicArray<date> res(std::distance(start, end));
-    for (int i = res.size() - 1; i > -1; --i) {
-        res[--cnt[get_key(start + i, fl)]] = *(start + i);
+    iterator ind = start;
+    for (size_t i = 0; i < buckets.size(); ++i) {
+        for (size_t j = 0; j < buckets[i].size(); ++j) {
+            *ind = std::move(buckets[i][j]);
+            ++ind;
+        }
     }
-    for (size_t i = 0; i < res.size(); ++i) {
-        *(start + i) = res[i];
-    }
-}
-
-template<typename iterator>
-void radix_sort(iterator begin, iterator end) {
-    count_sort(start, end, day);
-    count_sort(start, end, month);
-    count_sort(start, end, year);
-}
-
-std::istream& operator>>(std::istream& is, String& s) {    
-    char c;
-    while (is.get(c) && std::isspace(c)); 
-    if (!is) return is;
-    s.clear();
-    s.push_back(c);
-    while(is.get(c) && !std::isspace(c)) {
-        s.push_back(c);
-    }
-    return is;
-}
-
-std::ostream& operator<<(std::ostream& os, const String& s) {
-    for (char c : s) {
-        os << c;
-    }
-    return os;
 }
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    
-    MyVector<node> v;
-    node elem;
-    MyVector<date> vals;
-    while (std::cin >> elem.key >> elem.val) {
-        vals.push_back(date_to_numb(elem.key));
-        v.push_back(std::move(elem));
+    DynamicArray<elem> data;
+    elem el;
+    while (std::cin >> el.key) {
+        char c;
+		std::cin.get();
+        while (std::cin.get(c) && c != '\n') {
+            el.val.push_back(c);
+        }
+        data.push_back(std::move(el));
     }
-    
-    MyVector<size_t> order(v.size());
-    for (size_t i = 0; i < v.size(); ++i) {
-        order[i] = i;
+    bucket_sort(data.begin(), data.end());
+    for (const auto& [key, val] : data) {
+        std::cout << key << '\t';
+        for (char c : val) {
+            std::cout << c;
+        }
+        std::cout << '\n';
     }
     return 0;
 }
